293c55d2ce01163e3b65c968a28b0236b16745b5
nirojneupane10 <niroj.neupane@varicon.com.au>
2025-03-15 14:48:26 +0545

    feat: add user validation schema using zod and update auth constants for improved validation

--- src/common/auth/authConstants.js
+++ src/common/auth/authConstants.js
@@ -1,4 +1,11 @@
 export const UserAUthConstants = {
+  MIN_LENGTH: 1,
+  MAX_LENGTH: 50,
+  FIRST_NAME_REQUIRED: "First name is required",
+  LAST_NAME_REQUIRED: "Last name is required",
+  EMAIL_REQUIRED: "Email is required",
+  VALID_EMAIL_ERROR: "Invalid email",
+  PASSWORD_REQUIRED: "Password is required",
   PASSWORD_MIN_LENGTH: 6,
   USER_ALREADY_EXISTS: "User already exists",
   PASSWORD_LENGTH_ERROR: "Password must be atleast 6 characters long",
