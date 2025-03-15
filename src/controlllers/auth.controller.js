3fb0a2aa4ff4790f817704ec5baac98f1bcf0431
nirojneupane10 <niroj.neupane@varicon.com.au>
2025-03-15 14:19:56 +0545

    feat: refactor signUp route to use asyncHandler for improved error handling

--- src/controlllers/auth.controller.js
+++ src/controlllers/auth.controller.js
@@ -2,46 +2,42 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { UserAUthConstants } from "../common/constants.js";
 import { generateToken } from "../lib/utils.js";
+import asyncHandler from "../lib/asyncHandler.js";
 //Route 1: Sign Up route
-export const signUp = async (req, res) => {
+export const signUp = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   //check user passowrd length
-  try {
-    if (password.length < UserAUthConstants.PASSWORD_MIN_LENGTH) {
-      return res
-        .status(400)
-        .json({ message: UserAUthConstants.PASSWORD_LENGTH_ERROR });
-    }
-
-    //Check if user already exists
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res
-        .status(400)
-        .json({ message: UserAUthConstants.USER_ALREADY_EXISTS });
-    }
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    //Create user
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-    });
-    const token = generateToken({ id: user._id });
-    res.status(201).json({
-      message: UserAUthConstants.USER_CREATED,
-      token,
-      userId: user._id,
-      fullName: `${user.firstName} ${user.lastName}`,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: UserAUthConstants.SOMETHING_WENT_WRONG });
+  if (password.length < UserAUthConstants.PASSWORD_MIN_LENGTH) {
+    return res
+      .status(400)
+      .json({ message: UserAUthConstants.PASSWORD_LENGTH_ERROR });
   }
-};
+
+  //Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ message: UserAUthConstants.USER_ALREADY_EXISTS });
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  //Create user
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+  const token = generateToken({ id: user._id });
+  res.status(201).json({
+    message: UserAUthConstants.USER_CREATED,
+    token,
+    userId: user._id,
+    fullName: `${user.firstName} ${user.lastName}`,
+  });
+});
 export const login = (req, res) => {
   res.send("Sign up route");
 };
