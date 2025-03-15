6185a0e064565eb498ab0cafd0f6333cf452b712
nirojneupane10 <niroj.neupane@varicon.com.au>
2025-03-15 14:10:37 +0545

    fix: log error details in signUp route for better debugging

--- src/controlllers/auth.controller.js
+++ src/controlllers/auth.controller.js
@@ -40,6 +40,7 @@ export const signUp = async (req, res) => {
         user.firstName
       );
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: UserAUthConstants.SOMETHING_WENT_WRONG });
   }
 };
