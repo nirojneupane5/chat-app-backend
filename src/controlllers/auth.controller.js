14d374a60d4ed91173342602065612b219d28c59
nirojneupane10 <niroj.neupane@varicon.com.au>
2025-03-15 13:49:47 +0545

    fix: remove unnecessary response from signUp route

--- src/controlllers/auth.controller.js
+++ src/controlllers/auth.controller.js
@@ -1,6 +1,21 @@
+import User from "../models/user.model.js";
 //Route 1: Sign Up route
-export const signUp = (req, res) => {
-  res.json("Sign up route");
+export const signUp = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  //Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  //Create user
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+  res.status(201).json(user);
 };
 export const login = (req, res) => {
   res.send("Sign up route");
