8ec682f859aa00b20189e0c6ef7e938df35a50e3
nirojneupane10 <niroj.neupane@varicon.com.au>
2025-03-15 13:50:23 +0545

    fix: validate password length in signUp route

--- src/controlllers/auth.controller.js
+++ src/controlllers/auth.controller.js
@@ -3,6 +3,13 @@ import User from "../models/user.model.js";
 export const signUp = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
+  //check user passowrd length
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be atleast 6 characters long" });
+  }
+
   //Check if user already exists
   const userExists = await User.findOne({ email });
   if (userExists) {
